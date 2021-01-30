f8d1940395761171c90e4b8473bf80f1669609fd
rahul-somani <somani.classes@outlook.com>
2021-01-30 17:25:31 +0530

    first commit

---
+++ Day1/first.py
@@ -0,0 +1,20 @@
+# ctrl X get rid of the entire line
+
+num1 = 10
+num2 = 2
+'''
+print(num1+num2)   # 12  alt shift down arrow
+print(num1-num2)   # 8
+print(num1*num2)   # 20
+print(num1/num2)   # 5.0
+print(num1//num2)  # 5   Floor division operator
+'''
+
+print(num1, end='\t\t')
+print("Rahul Somani")
+print("'Rahul Somani'")
+print('"Rahul Somani"')
+
+# To get the power of a number
+
+# print(2**2)
---
+++ Day1/formatting-basics.py
@@ -0,0 +1,6 @@
+name = "somu"
+age = 34
+# print(name, age)
+print(f'Hi {name} you are {age} years old .')  # {} placeholder  f-string
+
+print("hello {} you are {} years old .".format(name, age))
